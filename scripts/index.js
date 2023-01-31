9c11b5bf5f31b7166a4f551f348a59dd229071ba
Diana Kovaleva <dianak654@gmail.com>
2023-01-31 21:23:16 +0300

    beginning

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,42 @@
 const popupElement = document.querySelector('.popup');
+
+// кнопка закрытия попапа
 const popupCloseButton = popupElement.querySelector('.popup__close-button');
+
+// кнопки открытия попапов
 const popupOpenButton = document.querySelector('.profile__edit-button');
+
 const popupFormElement = popupElement.querySelector('.popup__form');
+
+//редактирование данных профиля
 const nameInput = document.querySelector('.popup__field_change_name');
 const jobInput = document.querySelector('.popup__field_change_job');
 const name = document.querySelector('.profile__name');
 const job = document.querySelector('.profile__job');
 
 
-const openPopup = function() {
-    popupElement.classList.add('popup_opened');
+const openPopup = function(popup) {
+    popup.classList.add('popup_opened');
     nameInput.value = name.textContent;
     jobInput.value = job.textContent;
 }
 
-const closePopup = function() {
-    popupElement.classList.remove('popup_opened');
+const closePopup = function(popup) {
+    popup.classList.remove('popup_opened');
 }
 
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
     name.textContent = nameInput.value;
     job.textContent = jobInput.value;
-    closePopup();
+    closePopup(popupElement.closest);
 }
 
-popupOpenButton.addEventListener('click', openPopup);
-popupCloseButton.addEventListener('click', closePopup);
+popupOpenButton.addEventListener('click', function () {
+  openPopup(popupElement)
+})
+popupCloseButton.addEventListener('click', function() {
+  closePopup(popupElement)
+});
 
 popupFormElement.addEventListener('submit', handleFormSubmit); 
